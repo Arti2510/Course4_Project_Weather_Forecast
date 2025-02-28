c5026a1436ca34d92a46d152b8698423d69bd981
Arti2510 <artimaurya2510@gmail.com>
2025-02-28 11:34:51 +0530

    add javascript files to my project folder

---
+++ dateTime.js
@@ -0,0 +1,38 @@
+function dateTime(){
+
+    const months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+    
+    const days = [
+        'Sun', 'Mon', 'Tue', 'Wed',
+        'Thurs', 'Fri', 'Sat'
+    ];
+
+    const date_Time = new Date();
+
+    const year = date_Time.getFullYear();
+    const dayOfWeek = days[date_Time.getDay()];
+    const day = date_Time.getDate(); 
+    const month = months[date_Time.getMonth()]; 
+
+    let hours = date_Time.getHours();
+    const minutes = date_Time.getMinutes();
+    const seconds = date_Time.getSeconds();
+    
+
+    const amPM = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    document.getElementById("date").innerHTML = `${dayOfWeek}, ${day} ${month}, ${year}`;
+    document.getElementById("time").innerHTML = `${hours}:${formattedMinutes}:${formattedSeconds} ${amPM}`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setInterval(dateTime, 1000);
+    dateTime();
+  });
